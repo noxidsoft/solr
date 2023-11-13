b6041e483cefac0c9fa2f7234b19d70b41b454aa
Noxidsoft <noelmdixon@gmail.com>
2023-11-13 14:06:16 +1100

    X gitignore

--- solr.sh
+++ solr.sh
@@ -3,13 +3,13 @@
 wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
 wget https://dlcdn.apache.org/solr/solr/9.4.0/solr-9.4.0.tgz
 
-tar xzf ./openjdk-12.0.2_linux-x64_bin.tar.gz -C /usr/local/jdk-12.0.2/
-tar xzf ./solr-7.5.0.tgz -C /usr/local/solr-7.5.0/
+tar xzf ./jdk-21_linux-x64_bin.tar.gz -C /usr/local/jdk-21/
+tar xzf ./solr-9.4.0.tgz -C /usr/local/solr-9.4.0/
 
-sudo nano /usr/local/solr-7.5.0/bin/solr.in.sh
-# SET SOLR_JAVA_HOME="/usr/local/jdk-12.0.2" ]
+sudo nano /usr/local/solr-9.4.0/bin/solr.in.sh
+# SET SOLR_JAVA_HOME="/usr/local/jdk-21" ]
 # Press ctrl + x , y, <RETURN>
 
-/usr/local/solr-7.5.0/bin/solr start
+/usr/local/solr-9.4.0/bin/solr start
 
 # visit http://localhost:8983/solr/
