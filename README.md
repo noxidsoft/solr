4098a50d59df1b6a994789b9e90ba60a06448a71
Noxidsoft <noelmdixon@gmail.com>
2023-11-13 14:57:39 +1100

    X instruction and sh file locations

--- README.md
+++ README.md
@@ -3,3 +3,25 @@
 ```sh
 $ sh solr.sh
 ```
+
+When the editor opens, set `SOLR_JAVA_HOME="/usr/local/jdk-21.0.1"`
+
+To save, Press ctrl + x , y, <RETURN>
+
+If using Ubuntu/WSL2, you can kill ports if needed like this ($PORT is the port number if known):
+
+```sh
+$> lsof -ti :$PORT
+```
+
+then...
+
+```sh
+$> kill $(lsof -ti :$PORT)
+```
+
+or if parse SIGKILL in some cases (big hammer solution):
+
+```sh
+$> kill -9 $(lsof -ti :$PORT)
+```
--- solr.sh
+++ solr.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
+echo '\nWhen the editor opens, set SOLR_JAVA_HOME="/usr/local/jdk-21.0.1"'
+echo '\nPress ctrl + x , y, <RETURN>'
+
 wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
 wget https://dlcdn.apache.org/solr/solr/9.4.0/solr-9.4.0.tgz
 
-tar xzf ./jdk-21_linux-x64_bin.tar.gz -C /usr/local/jdk-21/
-tar xzf ./solr-9.4.0.tgz -C /usr/local/solr-9.4.0/
+sudo tar xzf ./jdk-21_linux-x64_bin.tar.gz -C /usr/local/
+sudo tar xzf ./solr-9.4.0.tgz -C /usr/local/
+
+rm ./jdk-21_linux-x64_bin.tar.gz
+rm ./solr-9.4.0.tgz
 
 sudo nano /usr/local/solr-9.4.0/bin/solr.in.sh
-# SET SOLR_JAVA_HOME="/usr/local/jdk-21" ]
+# set SOLR_JAVA_HOME="/usr/local/jdk-21.0.1"
 # Press ctrl + x , y, <RETURN>
 
 /usr/local/solr-9.4.0/bin/solr start
 
 # visit http://localhost:8983/solr/
+echo '\nVisit this URL: http://localhost:8983/solr/'
