9826949785edae37c965eda40146f65088c9ee27
Noxidsoft <noelmdixon@gmail.com>
2023-11-13 15:01:20 +1100

    X bring solr into user local directory This should solve the root auth that solr rejects. I kept the jdk in /usr/local so user still needs to update solr.in.sh

--- solr.sh
+++ solr.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
+# solr-9.4.0 tested as at 2023-11-13 on WSL2
 
 echo '\nWhen the editor opens, set SOLR_JAVA_HOME="/usr/local/jdk-21.0.1"'
-echo '\nPress ctrl + x , y, <RETURN>'
+echo '\nPress ctrl + x , y, <RETURN>\n'
 
 wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
 wget https://dlcdn.apache.org/solr/solr/9.4.0/solr-9.4.0.tgz
 
 sudo tar xzf ./jdk-21_linux-x64_bin.tar.gz -C /usr/local/
-sudo tar xzf ./solr-9.4.0.tgz -C /usr/local/
+sudo tar xzf ./solr-9.4.0.tgz -C ~/
 
 rm ./jdk-21_linux-x64_bin.tar.gz
 rm ./solr-9.4.0.tgz
 
-sudo nano /usr/local/solr-9.4.0/bin/solr.in.sh
+sudo nano ~/solr-9.4.0/bin/solr.in.sh
 # set SOLR_JAVA_HOME="/usr/local/jdk-21.0.1"
 # Press ctrl + x , y, <RETURN>
 
-/usr/local/solr-9.4.0/bin/solr start
+~/solr-9.4.0/bin/solr start
 
 # visit http://localhost:8983/solr/
-echo '\nVisit this URL: http://localhost:8983/solr/'
+echo '\nVisit this URL: http://localhost:8983/solr/\n'
