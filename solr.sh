731c40f411cc58fbdd1a5c19d340626dd0fe9bf4
Noxidsoft <noelmdixon@gmail.com>
2023-11-13 15:07:45 +1100

    X

--- solr.sh
+++ solr.sh
@@ -8,12 +8,12 @@ wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
 wget https://dlcdn.apache.org/solr/solr/9.4.0/solr-9.4.0.tgz
 
 sudo tar xzf ./jdk-21_linux-x64_bin.tar.gz -C /usr/local/
-sudo tar xzf ./solr-9.4.0.tgz -C ~/
+tar xzf ./solr-9.4.0.tgz -C ~/
 
 rm ./jdk-21_linux-x64_bin.tar.gz
 rm ./solr-9.4.0.tgz
 
-sudo nano ~/solr-9.4.0/bin/solr.in.sh
+nano ~/solr-9.4.0/bin/solr.in.sh
 # set SOLR_JAVA_HOME="/usr/local/jdk-21.0.1"
 # Press ctrl + x , y, <RETURN>
 
